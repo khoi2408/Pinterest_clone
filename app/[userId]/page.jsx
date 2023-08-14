ee3101e4e0ea4fc267b22ac2ee034de5778570f0
khoi le <lmkhoi2k@gmail.com>
2023-08-14 11:14:16 +0700

    read data firebase & ui profile user page

--- app/[userId]/page.jsx
+++ app/[userId]/page.jsx
@@ -1,12 +1,39 @@
 "use client"
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import { collection, getDocs,getDoc,doc, getFirestore, query, where } from 'firebase/firestore'
+import app from '../Shared/firebaseConfig'
+import UserInfo from '../components/UserInfo'
 
 function Profile({params}) {
+  const db = getFirestore(app)
+  const [userInfo,setUserInfo]=useState();
   useEffect(() => {
-    console.log(params.userId.replace('%40', '@'));
+    if(params.userId)
+    {
+      getUserInfo(params.userId.replace('%40', '@'))
+    }
   }, [params])
+
+  const getUserInfo=async(email)=>{
+    const docRef = doc(db, "user",email );
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      setUserInfo(docSnap.data())
+    } else {
+      // docSnap.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  }
+
   return (
-    <div>Profile</div>
+    <div>
+      {userInfo ? 
+      
+      <UserInfo userInfo={userInfo}/> 
+      : null
+      }
+    </div>
   )
 }
 
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -37,10 +37,13 @@ function Header() {
         />
         <button className='bg-black text-white p-2 px-4 rounded-full hidden md:block'>Home</button>
         <button className='font-semibold p-2 px-4 rounded-full'>Create</button>
-        <div className='bg-[#e9e9e9] p-3 flex gap-3 items-center rounded-full w-full hidden md:flex'>
-            <HiSearch className='text-[25px] text-gray-500 md:flex' />
-            <input type="text" placeholder='Search' className='bg-transparent outline-none' />
+
+        <div className='bg-[#e9e9e9] p-3 px-6 gap-3 items-center rounded-full w-full hidden md:flex'>
+            <HiSearch className='text-[25px] text-gray-500' />
+            <input type="text" placeholder='Search' className='bg-transparent outline-none w-full' />
         </div>
+
+            <HiSearch className='text-[25px] text-gray-500 md:hidden'/>
             <HiBell className='text-[25px] md:text-[40px] text-gray-500 cursor-pointer'/>
             <HiChat className='text-[25px] md:text-[40px] text-gray-500 cursor-pointer'/>
             {session?.user?
